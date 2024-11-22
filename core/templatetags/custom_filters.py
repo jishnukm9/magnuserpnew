972360f0d545116b7020816de984aa31681401f4
nexcrest <devlop@nexcrestit.com>
2024-11-22 10:24:06 +0530

    first commit

---
+++ core/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
+
+
+
+
+@register.filter
+def replaceunderscore(value):
+    return value.replace("_"," ")
